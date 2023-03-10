bcb62a37106fadcefa17e92c2d1a029f91e75b96
Gayoung Park <gayoungp@uw.edu>
2023-03-09 20:35:37 -0800

    Edit trial figure

--- trial.R
+++ trial.R
@@ -16,9 +16,10 @@ our_points_tbl <-
             .id = "specimen")
 
 our_points_tbl %>%
-  filter(specimen %in% our_data_files[1:130]) %>%
+  filter(specimen %in% our_data_files[1:40]) %>%
   ggplot() +
   aes(V1, V2) +
-  geom_point() +
+  geom_point(size =0.2) +
   facet_wrap(~ specimen) +
-  coord_fixed()
+  coord_fixed() +
+  theme(strip.background = element_blank(), strip.text = element_blank())
